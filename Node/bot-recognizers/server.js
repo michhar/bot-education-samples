1c3e5aa5cfea09f91eb5f83a94d2f04a05507c8d
Micheleen <michhar@microsoft.com>
2017-01-27 16:08:58 -0800

    Update server.js

--- Node/bot-recognizers/server.js
+++ Node/bot-recognizers/server.js
@@ -50,7 +50,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 // The model_url of format:  
 //   https://api.projectoxford.ai/luis/v2.0/apps/[model id goes here]?subscription-key=[key goes here]
 //  Replace the "" with your model url
-var model_url = "" || 'https://api.projectoxford.ai/luis/v2.0/apps/c413b2ef-382c-45bd-8ff0-f76d60e2a821?subscription-key=a807f61418f24f32ac9dfdae2bba2958';
+var model_url = "" || 'https://api.projectoxford.ai/luis/v2.0/apps/[model id goes here]?subscription-key=[key goes here];
 
 // Setup bot and default message handler
 var bot = new builder.UniversalBot(connector, function (session) {
