a2c37f930b5cfc7681f7a50c39eec743a42c5ee0
Micheleen <michhar@microsoft.com>
2017-01-27 16:09:43 -0800

    Update configuration.js

--- Node/bot-cognitive-sample/configuration.js
+++ Node/bot-cognitive-sample/configuration.js
@@ -5,7 +5,7 @@ var _config = {
     },
     COMPUTER_VISION_SERVICE: {
         API_URL: "https://api.projectoxford.ai/vision/v1.0/",
-        API_KEY: "4c1c05b58bbe4abe9616139fe8a04783"  //You can obtain an COGNITIVE SERVICE API KEY: https://www.microsoft.com/cognitive-services/en-us/pricing
+        API_KEY: "key-goes-here"  //You can obtain an COGNITIVE SERVICE API KEY: https://www.microsoft.com/cognitive-services/en-us/pricing
     }
 };
 exports.CONFIGURATIONS = _config;
